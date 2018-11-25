08b77f847baefe5e2ef27c6b426cc29564bd76d7
lkc311 <lkc311@163.com>
2018-11-26 00:05:14 +0800

    ADD

--- QICore.NIdentityServer4/Startup.cs
+++ QICore.NIdentityServer4/Startup.cs
@@ -8,14 +8,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using QICore.NIdentityServer4.Extensions;
 
 namespace QICore.NIdentityServer4
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env, IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+            Configuration = builder.Build();
+           // Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +30,21 @@ namespace QICore.NIdentityServer4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddServiceSingleton(Configuration);
             services.AddMvc();
+            #region 设置允许跨域请求
+            services.AddCors(options =>
+            {
+                // this defines a CORS policy called "default"
+                options.AddPolicy("AllowAllOrigin", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod().AllowCredentials();
+                });
+
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,8 +54,10 @@ namespace QICore.NIdentityServer4
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseIdentityServer();//【客户端模式】
 
             app.UseMvc();
+           
         }
     }
 }
--- QICore.NRedis/Startup.cs
+++ QICore.NRedis/Startup.cs
@@ -23,6 +23,9 @@ namespace QICore.NRedis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // 添加Options服务
+            services.AddOptions();           
             services.AddMvc();
         }
 
