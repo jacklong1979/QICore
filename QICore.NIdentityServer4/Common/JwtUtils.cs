f25090cf716457690b4356c3ce07983564a3656b
lkc311 <lkc311@163.com>
2018-12-16 22:00:06 +0800

    add

---
+++ QICore.NIdentityServer4/Common/JwtUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using QICore.NIdentityServer4.Options;
+
+namespace QICore.NIdentityServer4.Common
+{
+    public class JwtUtils: Controller
+    {
+        /// <summary>
+        /// 生成一个新的 Token
+        /// </summary>
+        /// <param name="option">身份配置信息，生成Token所需要的信息</param>
+        /// <param name="claims">用户信息实体</param>      
+        /// <returns></returns>
+        private JsonResult CreateToken(IdentityOption option, Claim[] claims)
+        {
+            #region Claim
+           // var claims = new Claim[]
+           //{
+
+           //     new Claim(JwtRegisteredClaimNames.Aud, _options.Audience),
+           //     new Claim(JwtRegisteredClaimNames.Sid, username),
+           //     new Claim(JwtRegisteredClaimNames.Sub, username),
+           //     new Claim(JwtRegisteredClaimNames.Exp, _options.ExpiresIn.ToString()),
+           //     new Claim(JwtRegisteredClaimNames.AuthTime, DateTime.Now.ToString()),
+           //     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+           //     new Claim(JwtRegisteredClaimNames.Iat, _options.Issued.ToString(), ClaimValueTypes.Integer64),//发行时间
+           //     //用户名
+           //     new Claim("UserName","中国人民"),
+           //     //角色
+           //     new Claim("Role","我是角色"),
+           //     new Claim("Country","中国"),
+           //     new Claim("Expired",_options.ExpiresIn.ToString()),
+           //     new Claim("Mobile","13556891160")
+           //};
+            #endregion
+            var jwt = new JwtSecurityToken(
+               issuer: option.Issuer,
+               audience: option.Audience,
+               claims: claims,
+               notBefore: DateTime.UtcNow,
+               expires: option.ExpiresTime,
+               signingCredentials: option.SigningCredentials);
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var token = new
+            {
+                access_token = encodedJwt,
+                token_type = option.TokenType,
+                expires_in = option.ExpiresIn, //过期时间(秒)
+                expires_time = option.ExpiresTime.ToString("yyyy-MM-dd HH:mm:ss"), //过期时间（日期）
+                claims = claims
+            };
+            return Json(token);
+        }
+    }
+}
---
+++ QICore.NIdentityServer4/Common/RSAUtils.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace QICore.NIdentityServer4.Common
+{
+    public class RSAUtils
+    {
+        /// <summary>
+        /// 从本地文件中读取用来签发 Token 的 RSA Key
+        /// </summary>
+        /// <param name="directoryPath">存放密钥的文件夹路径</param>
+        /// <param name="withPrivate">用【私有密钥】，如果是空的就用【公有密钥】</param>
+        /// <param name="keyParameters"></param>
+        /// <returns></returns>
+        public static bool TryGetKeyParameters(string directoryPath, bool withPrivate, out RSAParameters keyParameters)
+        {
+            string filename = withPrivate ? "key.private.rsa" : "key.public.rsa";
+            keyParameters = default(RSAParameters);
+            if (Directory.Exists(directoryPath) == false)
+            {
+                return false;
+            }
+            keyParameters = JsonConvert.DeserializeObject<RSAParameters>(File.ReadAllText(Path.Combine(directoryPath, filename)));
+            return true;
+        }
+
+        /// <summary>
+        /// 生成并保存 RSA 公钥与私钥
+        /// </summary>
+        /// <param name="directoryPath">存放密钥的文件夹路径</param>
+        /// <returns></returns>
+        public static RSAParameters GenerateAndSaveKey(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            RSAParameters publicKeys, privateKeys;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    privateKeys = rsa.ExportParameters(true);
+                    publicKeys = rsa.ExportParameters(false);
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+            File.WriteAllText(Path.Combine(directoryPath, "key.private.rsa"), JsonConvert.SerializeObject(privateKeys));
+            File.WriteAllText(Path.Combine(directoryPath, "key.public.rsa"), JsonConvert.SerializeObject(publicKeys));
+            return privateKeys;
+        }
+    }
+}
+
