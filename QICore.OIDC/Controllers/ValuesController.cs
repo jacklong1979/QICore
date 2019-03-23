7b31ea64f32298adef96d47a13606102bc493321
lkc311 <lkc311@163.com>
2019-03-23 23:59:55 +0800

    add

--- QICore.OIDC/Controllers/ValuesController.cs
+++ QICore.OIDC/Controllers/ValuesController.cs
@@ -2,44 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace QICore.OIDC.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    [Authorize]
+    public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            var user=this.User;
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET api/values
+        [HttpGet("userinfo")]      
+        public IActionResult GetUserinfo()
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            // var user = this.User.Claims.ToList();
+            var user = from c in User.Claims select new { type = c.Type, value = c.Value };
+            return Json(user);
         }
     }
 }
--- QICore.OIDC/Startup.cs
+++ QICore.OIDC/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using QICore.OIDC.Models;
 
 namespace QICore.OIDC
 {
@@ -24,6 +28,39 @@ namespace QICore.OIDC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //将appsettings.json中的JwtSettings部分文件读取到JwtSettings中，这是给其他地方用的
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+            //由于初始化的时候我们就需要用，所以使用Bind的方式读取配置
+            //将配置绑定到JwtSettings实例中
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind("JwtSettings", jwtSettings);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })            
+            .AddJwtBearer(o => {
+                //主要是jwt  token参数设置
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    //Token颁发机构
+                    ValidIssuer = jwtSettings.Issuer,
+                    //颁发给谁
+                    ValidAudience = jwtSettings.Audience,
+                    //这里的key要进行加密，需要引用Microsoft.IdentityModel.Tokens
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+                    ValidateIssuerSigningKey = true,
+                    //是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
+                    ValidateLifetime = true,
+                    //允许的服务器时间偏移量
+                    ClockSkew = TimeSpan.Zero
+
+                };
+            });
+
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -34,7 +71,7 @@ namespace QICore.OIDC
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
