9950c1e58059d5c656c3bca61bace3602563ed61
lkc311 <lkc311@163.com>
2019-10-07 23:25:51 +0800

    ADD

--- QICore.ElasticSearchCore/Program.cs
+++ QICore.ElasticSearchCore/Program.cs
@@ -3,7 +3,9 @@ using log4net.Config;
 using log4net.Repository;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nest;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace QICore.ElasticSearchCore
@@ -15,6 +17,38 @@ namespace QICore.ElasticSearchCore
 
             AutofacIoc.Register();
             test.write();
+
+            const int numberOfCycles = 10;
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(
+                node
+            ).DefaultIndex("people");
+            var client = new ElasticClient(settings);
+            //var searchResults = client.Search<Person>(s => s
+            //    .From(0)
+            //    .Size(10)
+            //    .Query(q => q
+            //         .Term(p => p.Firstname, "martijn")
+            //    )
+            //);
+           // var client = ElasticSearchBulk.GetElasticClient();
+            //var indexName = "people";//索引名称小写
+            //var createSuccess = ElasticSearchBulk.CreateIndex<TestNum>(client, indexName);
+            //if (createSuccess)
+            //{
+               
+
+             
+            //}
+            var list = new List<TestNum>();
+            for (var i = 5; i < numberOfCycles; i++)
+            {
+                var item = new TestNum { id = i, name = $"中国人民: {i} ", age = 10 * i };
+                list.Add(item);
+            }
+           // client.IndexMany<TestNum>(list);
+           ElasticSearchBulk.BulkAll(client, "people", list);
+
             Console.Read();
         }
     }
@@ -28,5 +62,11 @@ namespace QICore.ElasticSearchCore
         {
             return "功能点1";
         }
+    }
+    public class TestNum
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
     }
 }
