8b826980bb422a4c0c4ebbc6fae0f264660fbfb0
lkc311 <lkc311@163.com>
2020-01-30 10:48:10 +0800

    a

--- QICore.ElasticSearchCore.WebApi/Program.cs
+++ QICore.ElasticSearchCore.WebApi/Program.cs
@@ -17,7 +17,6 @@ namespace QICore.ElasticSearchCore.WebApi
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         /// <summary>
         /// ËõÖ÷Æô¶¯.
         /// </summary>
